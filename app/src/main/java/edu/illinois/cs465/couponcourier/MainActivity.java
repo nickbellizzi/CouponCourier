5052774444bd871e3749459c2458ffc10667c204
Eshan Dhar <eshandhar@gmail.com>
2021-11-16 14:18:07 -0600

    Removed coupon tab from nav bar, now opens when you click coupon

--- app/src/main/java/edu/illinois/cs465/couponcourier/MainActivity.java
+++ app/src/main/java/edu/illinois/cs465/couponcourier/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Pass the ID's of Different destinations
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_coupon, R.id.navigation_upload)
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_upload)
                 .build();
 
         //Initialize NavController.
